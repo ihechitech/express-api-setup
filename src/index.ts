c4a1e077b6267f17266b22c9cf9aa834e5626d33
Ihechi Chizitere Chukwuemeka <ihechichiziterechukwuemeka@gmail.com>
2026-02-01 01:21:07 +0100

    installed express and other dependencies

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { type Response, type Request } from "express";
+
+const app = express()
+
+const port = 3000
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World")
+})
+
+
+
+app.listen(port, () => {
+    console.log("App is running on port 3000...")
+})
